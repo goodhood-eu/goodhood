98055c76d7d1dc039cb21b1c1ca2daea3b41e432
Peter Haßler <pet.hassler@gmail.com>
2020-05-15 10:36:17 +0200

    Add import statements to preview and improve icon-details navigation

---
+++ packages/icons/preview/import_statements.jsx
@@ -0,0 +1,16 @@
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+
+const getIconName = (fileName) => fileName.substr(0, fileName.lastIndexOf('.'));
+
+export const importStatementForReactComponent = (size, fileName) => {
+  const name = getIconName(fileName);
+  const alias = upperFirst(camelCase(`${name}_icon`));
+  return `import { ReactComponent as ${alias} } from "@goodhood/icons/svg/${size}/${fileName}"`;
+}
+
+export const importStatementForImg = (size, fileName) => {
+  const name = getIconName(fileName);
+  const alias = camelCase(`${name}_icon`);
+  return `import ${alias} from "@goodhood/icons/svg/${size}/${name}"`;
+}
