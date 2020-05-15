f62231f9df2feec153f93ff99b5c3a05ee1ca941
Peter Haßler <pet.hassler@gmail.com>
2020-05-15 18:32:33 +0200

    Refactor: Extract alias helper util

--- packages/icons/preview/import_statements.jsx
+++ packages/icons/preview/import_statements.jsx
@@ -2,15 +2,22 @@ import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 
 const getIconName = (fileName) => fileName.replace(/\.svg$/, '');
+const getAlias = (iconName, suffix, startUpper) => {
+  let icon = camelCase(iconName);
+
+  if (startUpper) icon = upperFirst(icon);
+
+  return `${icon}${suffix}`
+}
 
 export const importStatementForReactComponent = (size, fileName) => {
   const name = getIconName(fileName);
-  const alias = upperFirst(camelCase(`${name}_icon`));
+  const alias = getAlias(name, 'Icon', true);
   return `import { ReactComponent as ${alias} } from "@goodhood/icons/svg/${size}/${fileName}"`;
 }
 
 export const importStatementForImg = (size, fileName) => {
   const name = getIconName(fileName);
-  const alias = camelCase(`${name}_icon`);
+  const alias = getAlias(name, 'Icon', false);
   return `import ${alias} from "@goodhood/icons/svg/${size}/${name}"`;
 }
