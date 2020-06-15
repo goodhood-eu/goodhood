947abd4e40ce8ee970a138b174ff45bd75a69e3d
Peter Haßler <pet.hassler@gmail.com>
2020-06-15 11:07:45 +0200

    Add show-source addon to storybook

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -30,7 +30,7 @@ const getStyleLoaders = ({ modules }) => (
 
 module.exports = {
   stories: ['../src/**/*.stories.jsx'],
-  addons: ['@storybook/addon-viewport/register'],
+  addons: ['@storybook/addon-viewport/register', '@storybook/addon-storysource'],
   webpackFinal: (config) => {
     config.module.rules.push({
       test: /\.scss$/,
