e9aa7d47410b78d21b4c96d0eed810b6ee2da3c2
Peter Haßler <pet.hassler@gmail.com>
2020-06-17 13:34:59 +0200

    Rename to match naming schema

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,4 +1,4 @@
 import pkg from './package.json';
-import baseConfig from '../../presets/react/rollup.config';
+import getConfig from '../../presets/react/rollup.config';
 
-export default baseConfig(pkg, __dirname);
+export default getConfig(pkg, __dirname);
