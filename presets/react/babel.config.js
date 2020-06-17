dda2e21a824725f4cf9f5c5e6e1995c3e561bed0
Peter Haßler <pet.hassler@gmail.com>
2020-06-17 07:56:38 +0200

    Move babel configuration to root package

---
+++ presets/react/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "presets": [
+    ["@babel/preset-env", {"modules": false}],
+    "@babel/preset-react"
+  ],
+  "plugins": [
+    "transform-node-env-inline",
+    "transform-react-remove-prop-types",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-strict-mode",
+    "@babel/plugin-proposal-json-strings"
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        ["@babel/preset-env", {
+          "useBuiltIns": "usage",
+          "corejs": "3"
+        }]
+      ]
+    }
+  }
+}
