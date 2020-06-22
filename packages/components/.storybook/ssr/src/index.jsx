9f7744dc40f012074d5096c170bd9647221ac1d4
Peter Haßler <pet.hassler@gmail.com>
2020-06-22 11:23:35 +0200

    Force iframe reload on story change to always show SSR results

--- packages/components/.storybook/ssr/src/index.jsx
+++ packages/components/.storybook/ssr/src/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import qs from 'qs';
+import { SET_CURRENT_STORY } from '@storybook/core-events';
+import addons from '@storybook/addons';
 import { hydrate } from 'react-dom';
 import { start } from '@storybook/core/client';
 import 'nebenan-ui-kit/styles.scss';
 import '../../../../../presets/react/storybook/preview.scss';
-import { getStories } from './utils';
+import { getStories, getUrlForStory } from './utils';
+
+let locationChangeInProgress = false;
 
 // /Users/peter/Documents/Sauce/storybook/app/react/src/client/preview/render.tsx
-const render = ({
-  storyFn: StoryFn,
-  ...args
-}) => {
-  console.warn(args);
+const render = ({ storyFn: StoryFn }) => {
+  if (locationChangeInProgress) return;
+
   hydrate(<StoryFn />, document.getElementById('root'));
 };
-// TODO: reload page on story change
+
+const forceServerSideRender = (story) => {
+  locationChangeInProgress = true;
+  window.location.assign(getUrlForStory(story));
+}
 
 const api = start(render);
-const configure = (...args) => api.configure(...args, 'react');
+api.configure(() => getStories(), module, 'react');
 
-configure(() => getStories(), module);
+const channel = addons.getChannel();
+channel.on(SET_CURRENT_STORY, forceServerSideRender);
--- packages/components/.storybook/ssr/src/utils.jsx
+++ packages/components/.storybook/ssr/src/utils.jsx
@@ -1,4 +1,18 @@
+import qs from 'qs';
+
 export const getStories = () => {
   const stories = require.context('../../../src', true, /\.stories\.jsx$/);
   return stories.keys().map(stories);
 };
+
+// /Users/peter/Documents/Sauce/storybook/lib/core/src/client/preview/url.js
+export const getUrlForStory = ({ storyId, viewMode }) => {
+  const { path, selectedKind, selectedStory, ...rest } = qs.parse(document.location.search, {
+    ignoreQueryPrefix: true,
+  })
+  return `${document.location.pathname}?${qs.stringify({
+    ...rest,
+    id: storyId,
+    viewMode,
+  })}`
+}
