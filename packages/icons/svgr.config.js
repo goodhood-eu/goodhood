f4bd7a95cd1c7cbeb3833402682902a85358225f
Peter Haßler <pet.hassler@gmail.com>
2020-06-25 19:32:07 +0200

    Add additional rules for babel to not require more transpiling

--- packages/icons/svgr.config.js
+++ packages/icons/svgr.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   svgo: true,
   plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+  jsx: {
+    babelConfig: {
+      plugins: [
+        ['@babel/plugin-transform-react-jsx', {
+          useBuiltIns: true,
+        }],
+      ],
+    },
+  },
 };
