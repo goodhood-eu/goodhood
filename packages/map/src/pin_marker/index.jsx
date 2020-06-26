48a30faefbb42b674bfde8414a12bf2f5ef8a02c
atomiomi <atomio.ak@gmail.com>
2020-06-26 18:14:53 +0600

    pin marker

--- packages/map/src/pin_marker/index.jsx
+++ packages/map/src/pin_marker/index.jsx
@@ -14,13 +14,28 @@ import {
 import Marker from '../marker';
 
 
+const types = [
+  PIN_MARKER_GREEN,
+  PIN_MARKER_BASE,
+  PIN_MARKER_ORANGE,
+  PIN_MARKER_YELLOW,
+  PIN_MARKER_RED,
+  PIN_MARKER_GRAY,
+  PIN_MARKER_BLUE,
+];
+
+const images = types.reduce((acc, type) => {
+  acc[type] = require(`./images/pin-${type}.svg`);
+  return acc;
+}, {});
+
 const PinMarker = ({
   children,
   type,
   ...rest
 }) => (
   <Marker {...rest}>
-    <img src={`/images/map/pin-${type}.svg`} alt="" />
+    <img src={images[type]} alt="" />
     {children}
   </Marker>
 );
@@ -31,15 +46,7 @@ PinMarker.defaultProps = {
 
 PinMarker.propTypes = {
   children: PropTypes.node,
-  type: PropTypes.oneOf([
-    PIN_MARKER_GREEN,
-    PIN_MARKER_BASE,
-    PIN_MARKER_ORANGE,
-    PIN_MARKER_YELLOW,
-    PIN_MARKER_RED,
-    PIN_MARKER_GRAY,
-    PIN_MARKER_BLUE,
-  ]),
+  type: PropTypes.oneOf(types),
 };
 
 export default PinMarker;
