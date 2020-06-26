91dda51266f679dda445bf37775db2a7501275bc
Peter Haßler <pet.hassler@gmail.com>
2020-06-26 10:44:28 +0200

    Refactor

--- packages/icons/preview/import_statements.jsx
+++ packages/icons/preview/import_statements.jsx
@@ -2,7 +2,6 @@ import upperFirst from 'lodash/upperFirst';
 import camelCase from 'lodash/camelCase';
 import snakeCase from 'lodash/snakeCase';
 
-const getIconName = (fileName) => fileName.replace(/\.svg$/, '');
 const getAlias = (iconName, suffix, startUpper) => {
   let icon = camelCase(iconName);
 
@@ -11,29 +10,24 @@ const getAlias = (iconName, suffix, startUpper) => {
   return `${icon}${suffix}`;
 };
 
-const getFileName = (sourceName) => (
-  snakeCase(sourceName)
-);
-
-const stripSVGExtension = (fileName) => (
-  fileName.replace(/\.svg$/, '')
-);
+const getTargetFileName = snakeCase;
+const stripSVGExtension = (fileName) => fileName.replace(/\.svg$/, '');
 
 export const importAliasForReactComponent = (filename) => (
-  getAlias(getIconName(filename), 'Icon', true)
+  getAlias(stripSVGExtension(filename), 'Icon', true)
 );
 
 export const importStatementForReactComponent = (size, fileName) => {
-  const name = getIconName(fileName);
+  const name = stripSVGExtension(fileName);
   const alias = getAlias(name, 'Icon', true);
-  const targetFileName = getFileName(
-    stripSVGExtension(fileName),
-  );
+  const targetFileName = getTargetFileName(name);
+
   return `import ${alias} from '@goodhood/icons/lib/${size}/${targetFileName}';`;
 };
 
 export const importStatementForImg = (size, fileName) => {
-  const name = getIconName(fileName);
+  const name = stripSVGExtension(fileName);
   const alias = getAlias(name, 'Icon', false);
-  return `import ${alias} from '@goodhood/icons/lib/${size}/${getFileName(name)}.svg';`;
+
+  return `import ${alias} from '@goodhood/icons/lib/${size}/${getTargetFileName(name)}.svg';`;
 };
