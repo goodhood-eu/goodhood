f1e44c169abdbec3ca72ee05f0f2be1fb2c1b520
Peter Haßler <pet.hassler@gmail.com>
2020-07-08 12:36:12 +0200

    Change icon component name to improve IDE support

--- packages/icons/utils/naming.js
+++ packages/icons/utils/naming.js
@@ -4,7 +4,7 @@ const snakeCase = require('lodash/snakeCase');
 
 const getSourceIconName = (fileName) => fileName.replace(/\.svg$/, '');
 const getIconName = (fileName) => camelCase(getSourceIconName(fileName));
-const getComponentName = (fileName) => upperFirst(getIconName(fileName));
+const getComponentName = (fileName) => `${upperFirst(getIconName(fileName))}Icon`;
 const getLibFileName = (fileName) => snakeCase(getSourceIconName(fileName));
 const getLibSvgFileName = (fileName) => `${getLibFileName(fileName)}.svg`;
 const getLibJsFileName = (fileName) => `${getLibFileName(fileName)}.js`;
