7c870242c2ad923d57fa298c6b70ba8ecfb4f5cf
Abylay Keldibek <atomio.ak@gmail.com>
2020-07-14 03:13:54 +0600

    Map fix (#15)

---
+++ packages/map/src/index.jsx
@@ -0,0 +1,11 @@
+export { default as Map } from './map';
+export { default as Polygon } from './polygon';
+export { default as Circle } from './circle';
+
+export { default as Marker } from './marker';
+export { default as CircleMarker } from './circle_marker';
+export { default as EyecatcherMarker } from './eyecatcher_marker';
+export { default as ImageMarker } from './image_marker';
+export { default as InfoMarker } from './info_marker';
+export { default as LabelMarker } from './label_marker';
+export { default as PinMarker } from './pin_marker';
