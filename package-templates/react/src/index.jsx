ed07995516ec03ee97dc0d8d8e6e41a28840bb5b
Peter Haßler <34499633+peterhass@users.noreply.github.com>
2020-07-14 12:30:42 +0200

    CircleCI and stylelint integration (#14)
    * Add basic circleci setup
    
    * Add stylelint (WIP)
    
    * Try out github workflows
    
    * Try to get it running
    
    * Fix name
    
    * Move all linting and test scripts to root package
    
    * Revert test errors

--- package-templates/react/src/index.jsx
+++ package-templates/react/src/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { addNumbers, getPackageName, getRootPackageName } from './utils';
+import styles from './index.module.scss';
 
 const Index = ({ numberA = 0, numberB = 0 }) => (
   <>
     <pre>addNumbers({numberA}, {numberB}) = {addNumbers(numberA, numberB)}</pre>
     <pre>Root package: {getRootPackageName()}</pre>
-    <pre>package: {getPackageName()}</pre>
+    <pre className={styles.highlight}>package: {getPackageName()}</pre>
   </>
 );
 
