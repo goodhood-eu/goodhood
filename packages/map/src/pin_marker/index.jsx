26c3f679594663436477eb1df0eea2f79fa404eb
Peter Haßler <pet.hassler@gmail.com>
2020-08-26 19:11:30 +0200

    Fix loading of PinMarker images

--- packages/map/src/pin_marker/index.jsx
+++ packages/map/src/pin_marker/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import pinBase01 from './images/pin-base-01.svg';
+import pinBlue01 from './images/pin-blue-01.svg';
+import pinGray04 from './images/pin-gray-04.svg';
+import pinGreen02 from './images/pin-green-02.svg';
+import pinOrange01 from './images/pin-orange-01.svg';
+import pinRed01 from './images/pin-red-01.svg';
+import pinYellow01 from './images/pin-yellow-01.svg';
 
 import {
   PIN_MARKER_GREEN,
@@ -14,7 +21,7 @@ import {
 import Marker from '../marker';
 
 
-const types = [
+const TYPES = [
   PIN_MARKER_GREEN,
   PIN_MARKER_BASE,
   PIN_MARKER_ORANGE,
@@ -24,11 +31,15 @@ const types = [
   PIN_MARKER_BLUE,
 ];
 
-// TODO: move to lib on build step
-// const images = types.reduce((acc, type) => {
-//   acc[type] = require(`./images/pin-${type}.svg`);
-//   return acc;
-// }, {});
+const IMAGE_MAP = {
+  [PIN_MARKER_GREEN]: pinGreen02,
+  [PIN_MARKER_BASE]: pinBase01,
+  [PIN_MARKER_ORANGE]: pinOrange01,
+  [PIN_MARKER_YELLOW]: pinYellow01,
+  [PIN_MARKER_RED]: pinRed01,
+  [PIN_MARKER_GRAY]: pinGray04,
+  [PIN_MARKER_BLUE]: pinBlue01,
+};
 
 const PinMarker = ({
   children,
@@ -36,7 +47,7 @@ const PinMarker = ({
   ...rest
 }) => (
   <Marker {...rest}>
-    <img src={type} alt={type} />
+    <img src={IMAGE_MAP[type]} alt={type} />
     {children}
   </Marker>
 );
@@ -47,7 +58,7 @@ PinMarker.defaultProps = {
 
 PinMarker.propTypes = {
   children: PropTypes.node,
-  type: PropTypes.oneOf(types),
+  type: PropTypes.oneOf(TYPES),
 };
 
 export default PinMarker;
