8dd8a484999e4bc7f94d0f8539602ccd382542f3
Peter Haßler <pet.hassler@gmail.com>
2020-09-10 10:50:05 +0200

    Fix circle calculation

--- packages/map/src/circle/utils.jsx
+++ packages/map/src/circle/utils.jsx
@@ -23,35 +23,22 @@ const STYLES = {
 
 const EARTH_CIRCUMFERENCE = 2 * Math.PI * 6378137;
 const MAPBOX_TILE_WIDTH = 512;
-const getPixels = (latitude, meters, zoomLevel) => {
+export const getPixels = (latitude, meters, zoomLevel) => {
   // http://blog.madebylotus.com/blog/creating-static-distance-circles-in-map-box-how-many-miles-are-in-a-pixel
   const mapWidth = MAPBOX_TILE_WIDTH * (2 ** zoomLevel);
-
   const meterPerPixel = Math.cos(latitude * Math.PI / 180) * EARTH_CIRCUMFERENCE / mapWidth;
   const pixelPerMeter = 1 / meterPerPixel;
-  // meterPerPixel pretty much match what https://docs.mapbox.com/help/glossary/zoom-level/ says
-  // but the circle is still wrong - a lot
 
-  console.log({ zoomLevel, pixelPerMeter, meterPerPixel });
-
-  return meters * meterPerPixel;
+  return meters * pixelPerMeter;
 };
-
-console.log('r', getPixels(0, 1, 0));
-console.log('r', getPixels(0, 1, 22));
 
 export const getPaint = (type, center, radius) => {
   const [, lat] = center;
 
-  console.log([
-    0, getPixels(lat, radius, 0),
-    22, getPixels(lat, radius, 22),
-  ]);
-
   return ({
     ...STYLES[type],
     'circle-radius': [
-      'interpolate', ['linear'], ['zoom'],
+      'interpolate', ['exponential', 2], ['zoom'],
       0, getPixels(lat, radius, 0),
       22, getPixels(lat, radius, 22),
     ],
--- packages/map/src/circle/utils.test.jsx
+++ packages/map/src/circle/utils.test.jsx
@@ -1,8 +1,19 @@
 import { assert } from 'chai';
-import { getGeoJSON } from './utils';
+import { getGeoJSON, getPixels } from './utils';
 
+const getMeterPerPixel = (pixelPerMeter) => 1 / pixelPerMeter;
 
 describe('circle/utils', () => {
+  it('getPixels', () => {
+    // compare with table at https://docs.mapbox.com/help/glossary/zoom-level/
+    const PRECISION = 0.1;
+
+    assert.approximately(getMeterPerPixel(getPixels(0, 1, 0)), 78271.484, PRECISION);
+    assert.approximately(getMeterPerPixel(getPixels(0, 1, 22)), 0.019, PRECISION);
+    assert.approximately(getMeterPerPixel(getPixels(40, 1, 0)), 59959.436, PRECISION);
+    assert.approximately(getMeterPerPixel(getPixels(40, 1, 10)), 58.554, PRECISION);
+  });
+
   describe('getGeoJSON', () => {
     it('generate geojson of circle', () => {
       assert.deepEqual(getGeoJSON([1, 2]), {
