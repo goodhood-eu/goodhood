76f02507b70b5a021d5e3720095e29cc8dbebf33
Peter Haßler <pet.hassler@gmail.com>
2020-09-28 10:41:02 +0200

    Add test for getReactionsCount helper function

--- packages/components/src/reactions_row/utils.test.jsx
+++ packages/components/src/reactions_row/utils.test.jsx
@@ -1,5 +1,5 @@
 import { assert } from 'chai';
-import { getSortedReactionKeys } from './utils';
+import { getReactionsCount, getSortedReactionKeys } from './utils';
 import { REACTION_GOOD_IDEA, REACTION_LOVE, REACTION_THANK_YOU } from '../constants';
 
 describe('ui/reaction_counter/utils', () => {
@@ -9,6 +9,13 @@ describe('ui/reaction_counter/utils', () => {
         [REACTION_LOVE]: 1, [REACTION_THANK_YOU]: 5, [REACTION_GOOD_IDEA]: 2,
       }),
       [REACTION_LOVE, REACTION_GOOD_IDEA, REACTION_THANK_YOU],
+    );
+  });
+
+  it('getReactionsCount', () => {
+    assert.equal(
+      getReactionsCount({ [REACTION_LOVE]: 2, [REACTION_THANK_YOU]: 1 }),
+      3,
     );
   });
 });
