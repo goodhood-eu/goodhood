8338be7d603fcbd56670792c6b4829b91c9352c2
Sergei Ozerov <ozerovsa@gmail.com>
2020-10-23 19:16:00 +0300

    remove unnecessary stuff

--- packages/components/src/tooltip/utils.jsx
+++ packages/components/src/tooltip/utils.jsx
@@ -3,18 +3,10 @@ import {
   POSITION_BOTTOM,
   POSITION_LEFT,
   POSITION_RIGHT,
-  TRIGGER_HOVER,
-  TRIGGER_CLICK,
+  TOOLTIP_OFFSET,
+  TOOLTIP_FLIP_SCREEN_OFFSET,
 } from './constants';
 
-const TOOLTIP_OFFSET = 10;
-const TOOLTIP_FLIP_SCREEN_OFFSET = 10;
-
-export const getTriggerProps = (trigger, handler) => {
-  if (trigger === TRIGGER_HOVER) return { onMouseEnter: handler };
-  if (trigger === TRIGGER_CLICK) return { onClick: handler };
-  return {};
-};
 
 export const getPopperOptions = (ref, position) => ({
   placement: position,
