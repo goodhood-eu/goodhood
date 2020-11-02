fdee3afa9a8ff7d2fbe361026b20cd2646781ac6
Sergei Ozerov <ozerovsa@gmail.com>
2020-11-02 19:50:52 +0300

    Added knobs

--- packages/components/src/tooltip/index.stories.jsx
+++ packages/components/src/tooltip/index.stories.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { withKnobs } from '@storybook/addon-knobs';
+import { text, withKnobs } from '@storybook/addon-knobs';
 import Tooltip from './index';
 import styles from './index.stories.module.scss';
 
-const tooltipShort = 'Design is like a joke.';
+const TOOLTIP_PLACEHOLDER = 'Design is like a joke.';
 
 export default { title: 'Tooltip', component: Tooltip, decorators: [withKnobs] };
 
+const tooltipText = text('Tooltip text', TOOLTIP_PLACEHOLDER);
+
 export const Default = () => (
   <div className={styles.container}>
-    <Tooltip text={tooltipShort}>Tooltip top</Tooltip>
-    <Tooltip type="bottom" text={tooltipShort}>Tooltip bottom</Tooltip>
-    <Tooltip type="left" text={tooltipShort}>Tooltip left</Tooltip>
-    <Tooltip type="right" text={tooltipShort}>Tooltip right</Tooltip>
+    <Tooltip text={tooltipText}>Tooltip top</Tooltip>
+    <Tooltip type="bottom" text={tooltipText}>Tooltip bottom</Tooltip>
+    <Tooltip type="left" text={tooltipText}>Tooltip left</Tooltip>
+    <Tooltip type="right" text={tooltipText}>Tooltip right</Tooltip>
   </div>
 );
