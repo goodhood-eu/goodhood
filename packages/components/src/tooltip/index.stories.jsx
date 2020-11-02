2a4eeae055c93af92c9e87dc802b2c01772ee3cf
Sergei Ozerov <ozerovsa@gmail.com>
2020-11-02 19:53:08 +0300

    Added knobs

--- packages/components/src/tooltip/index.stories.jsx
+++ packages/components/src/tooltip/index.stories.jsx
@@ -7,13 +7,11 @@ const TOOLTIP_PLACEHOLDER = 'Design is like a joke.';
 
 export default { title: 'Tooltip', component: Tooltip, decorators: [withKnobs] };
 
-const tooltipText = text('Tooltip text', TOOLTIP_PLACEHOLDER);
-
 export const Default = () => (
   <div className={styles.container}>
-    <Tooltip text={tooltipText}>Tooltip top</Tooltip>
-    <Tooltip type="bottom" text={tooltipText}>Tooltip bottom</Tooltip>
-    <Tooltip type="left" text={tooltipText}>Tooltip left</Tooltip>
-    <Tooltip type="right" text={tooltipText}>Tooltip right</Tooltip>
+    <Tooltip text={text('Tooltip text', TOOLTIP_PLACEHOLDER)}>Tooltip top</Tooltip>
+    <Tooltip type="bottom" text={text('Tooltip text', TOOLTIP_PLACEHOLDER)}>Tooltip bottom</Tooltip>
+    <Tooltip type="left" text={text('Tooltip text', TOOLTIP_PLACEHOLDER)}>Tooltip left</Tooltip>
+    <Tooltip type="right" text={text('Tooltip text', TOOLTIP_PLACEHOLDER)}>Tooltip right</Tooltip>
   </div>
 );
