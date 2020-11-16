7c22ef6172255583cf82a63dd9ffe0e4defc97dc
Peter Haßler <pet.hassler@gmail.com>
2020-11-16 10:11:44 +0100

    Fix build for packages without stylesheets

--- packages/chargebee/src/index.jsx
+++ packages/chargebee/src/index.jsx
@@ -1,6 +1,3 @@
-// TODO: Peter, build fails if package does not include any styles
-import './index.module.scss';
-
 export { default as Action } from './action';
 export { default as PortalLink } from './portal_link';
 export { default as Checkout } from './checkout';
