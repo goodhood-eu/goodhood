b55bcf53e2af61a930aa0e62b82e35f5a12b0679
Sergei Ozerov <ozerovsa@gmail.com>
2020-12-03 14:47:36 +0300

    lint fix

--- packages/components/src/base_tooltip/index.jsx
+++ packages/components/src/base_tooltip/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { createPopper } from '@popperjs/core';
 import { getPopperOptions } from './utils';
