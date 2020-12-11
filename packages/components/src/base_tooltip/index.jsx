f1d056a31fa5455ba1465309c65e20acfa19f235
Sergei Ozerov <ozerovsa@gmail.com>
2020-12-11 15:43:10 +0300

    fix

--- packages/components/src/base_tooltip/index.jsx
+++ packages/components/src/base_tooltip/index.jsx
@@ -1,4 +1,57 @@
-import Tooltip from './tooltip';
+import React, { useRef, useLayoutEffect } from 'react';
+import PropTypes from 'prop-types';
+import { createPopper } from '@popperjs/core';
+import { getPopperOptions } from './utils';
+
+const BaseTooltip = (props) => {
+  const {
+    position,
+    bubble,
+    children,
+    tooltipClassName,
+    arrowClassName,
+    contentClassName,
+    ...cleanProps
+  } = props;
+
+  const content = useRef(null);
+  const tooltip = useRef(null);
+  const arrow = useRef(null);
+
+  useLayoutEffect(() => {
+    const popper = createPopper(
+      content.current,
+      tooltip.current,
+      getPopperOptions(arrow.current, position),
+    );
+    return () => {
+      if (popper) popper.destroy();
+    };
+  }, [position]);
+
+  return (
+    <span {...cleanProps}>
+      <span ref={content} className={contentClassName}>{children}</span>
+      <span ref={tooltip} className={tooltipClassName}>
+        {bubble}
+        <i ref={arrow} className={arrowClassName} />
+      </span>
+    </span>
+  );
+};
+
+BaseTooltip.defaultProps = {
+  position: 'top',
+};
+
+BaseTooltip.propTypes = {
+  position: PropTypes.string.isRequired,
+  bubble: PropTypes.node,
+  children: PropTypes.node,
+  contentClassName: PropTypes.string,
+  tooltipClassName: PropTypes.string,
+  arrowClassName: PropTypes.string,
+};
 
 export * from './constants';
-export default Tooltip;
+export default BaseTooltip;
