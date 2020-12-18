f52a31fd4cdc62d5dd874415873c685db677e124
Sergei Ozerov <ozerovsa@gmail.com>
2020-12-18 17:00:12 +0300

    Removed react

--- packages/components/src/base_tooltip/index.jsx
+++ packages/components/src/base_tooltip/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { createPopper } from '@popperjs/core';
 import clsx from 'clsx';
