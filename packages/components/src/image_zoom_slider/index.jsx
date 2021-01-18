5c66a06113676dbd21947cc4966e3194bf754e04
Peter Haßler <pet.hassler@gmail.com>
2021-01-18 14:24:04 +0100

    Change ImageZoomSlider to have plus and minus (new!) icons next to it

--- packages/components/src/image_zoom_slider/index.jsx
+++ packages/components/src/image_zoom_slider/index.jsx
@@ -1,12 +1,16 @@
 import { useCallback, useContext, useRef } from 'react';
 import { useStateControlledInput } from 'nebenan-form/lib/base';
+import clsx from 'clsx';
+import Plus1OutlineIcon from '@goodhood/icons/lib/20x20/plus_1_outline';
+import MinusIcon from '@goodhood/icons/lib/20x20/minus';
+import styles from './index.module.scss';
 import Slider from '../slider';
 import Context from '../image_zoom_provider/context';
 
 
 const GET_EMPTY_LABEL = () => '';
 
-const ImageZoomSlider = (props) => {
+const ImageZoomSlider = ({ className, ...rest }) => {
   const {
     defaultScale,
     maxScale,
@@ -27,16 +31,20 @@ const ImageZoomSlider = (props) => {
   }, [previewSize, onAnchorZoom]);
 
   return (
-    <Slider
-      {...props}
-      ref={ref}
-      key={`${defaultScale}-${maxScale}`}
-      min={defaultScale}
-      max={maxScale}
-      step={0.01}
-      getLabel={GET_EMPTY_LABEL}
-      onUpdate={handleZoomSlider}
-    />
+    <article {...rest} className={clsx(styles.root, className)}>
+      <MinusIcon className={styles.label} />
+      <Slider
+        className={styles.slider}
+        ref={ref}
+        key={`${defaultScale}-${maxScale}`}
+        min={defaultScale}
+        max={maxScale}
+        step={0.01}
+        getLabel={GET_EMPTY_LABEL}
+        onUpdate={handleZoomSlider}
+      />
+      <Plus1OutlineIcon className={styles.label} />
+    </article>
   );
 };
 
