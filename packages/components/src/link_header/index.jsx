910c5c5c70c5d3338ff9aa84fac4cff92266c49c
Peter Haßler <34499633+peterhass@users.noreply.github.com>
2021-01-20 13:44:03 +0100

    Add LinkHeader and add new label prop (#76)
    * Migrate LinkHeader to @goodhood/components
    
    * Add label prop
    
    * Fix linter warnings
    
    * Refactor
    
    * Bump version
    
    * Rename label to action
    
    * Fix version

---
+++ packages/components/src/link_header/index.jsx
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { Link } from 'react-router-dom';
+import ArrowLeftIcon from '@goodhood/icons/lib/16x16/arrow_left';
+import ArrowRightIcon from '@goodhood/icons/lib/16x16/arrow_right';
+import styles from './index.module.scss';
+
+const LinkHeader = ({
+  children,
+  to,
+  onClick,
+  action,
+  reversed,
+  className: passedClassName,
+  ...cleanProps
+}) => {
+  const isClickable = to || onClick;
+
+  const className = clsx(styles.root, passedClassName, {
+    [styles.isReversed]: reversed,
+    [styles.isClickable]: isClickable,
+  });
+
+  let icon;
+  if (isClickable) {
+    const Icon = reversed ? ArrowLeftIcon : ArrowRightIcon;
+
+    icon = (
+      <div className={styles.navigation}>
+        {action && <span className={styles.action}>{action}</span>}
+        <Icon className={styles.icon} />
+      </div>
+    );
+  }
+
+  const content = (
+    <>
+      <div className={styles.children}>{children}</div>
+      {icon}
+    </>
+  );
+
+  if (!to) {
+    return <span {...cleanProps} {...{ onClick, className }}>{content}</span>;
+  }
+
+  return <Link {...cleanProps} {...{ to, className, onClick }}>{content}</Link>;
+};
+
+LinkHeader.propTypes = {
+  className: PropTypes.string,
+  to: PropTypes.string,
+  onClick: PropTypes.func,
+  reversed: PropTypes.bool,
+  children: PropTypes.node,
+  action: PropTypes.node,
+};
+
+export default LinkHeader;
---
+++ packages/components/src/link_header/index.stories.jsx
@@ -0,0 +1,47 @@
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import { MemoryRouter } from 'react-router';
+import LinkHeader from './index';
+
+export default { title: 'LinkHeader', component: LinkHeader, decorators: [withKnobs] };
+
+export const WithRouter = () => (
+  <MemoryRouter>
+    <LinkHeader
+      reversed={boolean('reversed', undefined)}
+      action={text('action', undefined)}
+      to={text('to', '/')}
+    >
+      <h3>Header</h3>
+      Content!
+    </LinkHeader>
+  </MemoryRouter>
+);
+
+export const WithOnClick = () => (
+  <LinkHeader
+    reversed={boolean('reversed', undefined)}
+    action={text('action', undefined)}
+    onClick={action('onClick')}
+  >
+    <h3>Header</h3>
+    Content!
+  </LinkHeader>
+);
+
+export const Empty = () => (
+  <LinkHeader>
+    <h3>Header</h3>
+    Content!
+  </LinkHeader>
+);
+
+export const WithActionNode = () => (
+  <LinkHeader
+    onClick={action('onClick')}
+    action={<span className="ui-link">Check it out</span>}
+  >
+    <h3>Header HeaderHeader HeaderHeader HeaderHeader Header    Header Header </h3>
+    Content!
+  </LinkHeader>
+);
