81a330add32faf5f7aca868c1cb667718d071c76
Peter Haßler <pet.hassler@gmail.com>
2021-05-20 09:54:39 +0200

    Add less horrible looking layout

--- .preview/src/components/layout/index.jsx
+++ .preview/src/components/layout/index.jsx
@@ -4,8 +4,8 @@ import Logo from '../../../../packages/components/src/logo';
 const Layout = ({ children }) => (
   <article className={styles.root}>
     <header className={styles.header}>
+      <h1>@goodhood/insert-package-name preview</h1>
       <Logo />
-      <h1>@goodhood preview</h1>
     </header>
     <div className={styles.content}>
       {children}
