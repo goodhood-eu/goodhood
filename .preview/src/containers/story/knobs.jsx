20c622b1dc84abc603e709ad4fe121271ca66e15
Peter Haßler <pet.hassler@gmail.com>
2021-05-25 18:24:06 +0200

    Add all other controls

--- .preview/src/containers/story/knobs.jsx
+++ .preview/src/containers/story/knobs.jsx
@@ -1,5 +1,5 @@
 import { useControl, useKnob, useKnobsContext } from '../../modules/knobs/hooks';
-import { NUMBER, TEXT } from '../../modules/knobs/constants';
+import { BOOLEAN, BUTTON, NUMBER, SELECT, TEXT } from '../../modules/knobs/constants';
 
 const TextControl = ({ id }) => {
   const [value, setValue] = useControl(id);
@@ -36,10 +36,68 @@ const NumberControl = ({ id }) => {
   );
 };
 
+const ButtonControl = ({ id }) => {
+  const { label, handler } = useKnob(id);
+
+  return (
+    <span className="ui-button" onClick={handler}>
+      {label}
+    </span>
+  );
+};
+
+const BooleanControl = ({ id }) => {
+  const [value, setValue] = useControl(id);
+  const knob = useKnob(id);
+
+  const handleInput = (e) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  return (
+    <label>
+      {knob.label}
+      <select value={value} onChange={handleInput} className="ui-input">
+        <option value>true</option>
+        <option value={false}>false</option>
+      </select>
+    </label>
+  );
+};
+
+const SelectControl = ({ id }) => {
+  const [value, setValue] = useControl(id);
+  const { label, options } = useKnob(id);
+
+  const handleInput = (e) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+  };
+
+  const renderOption = (optionLabel) => (
+    <option key={optionLabel} value={options[optionLabel]}>
+      {optionLabel}
+    </option>
+  );
+
+  return (
+    <label>
+      {label}
+      <select value={value} onChange={handleInput} className="ui-input">
+        {Object.keys(options).map(renderOption)}
+      </select>
+    </label>
+  );
+};
+
 
 const Controls = {
   [NUMBER]: NumberControl,
   [TEXT]: TextControl,
+  [BUTTON]: ButtonControl,
+  [BOOLEAN]: BooleanControl,
+  [SELECT]: SelectControl,
 };
 
 const Knobs = ({ className }) => {
