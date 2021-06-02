6e26a0ab25baa76d6130009b623c0ed44da396e7
Peter Haßler <pet.hassler@gmail.com>
2021-06-02 16:04:12 +0200

    Refactor

--- .preview/src/containers/error404/index.jsx
+++
@@ -1,5 +0,0 @@
-const Error404 = () => (
-  <h1>Error 404</h1>
-);
-
-export default Error404;
--- .preview/src/routes.jsx
+++ .preview/src/routes.jsx
@@ -1,12 +1,9 @@
 import { Route, Switch } from 'react-router';
 import Story from './containers/story';
-import Error404 from './containers/error404';
 
 export default () => (
   <Switch>
     <Route path="/" component={Story} exact />
     <Route path="/:id" component={Story} />
-
-    <Route component={Error404} />
   </Switch>
 );
