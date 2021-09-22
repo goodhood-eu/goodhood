03ba1b3eecbe756986f22ea4eaf1383ab39f6c6a
peterhass <34499633+peterhass@users.noreply.github.com>
2021-09-22 08:48:00 +0000

    Deploying to gh-pages from @ goodhood-eu/goodhood@98cbd2ad6075218415b4eb20946055c262c36b6e 🚀

--- packages/tracking/src/trigger.stories.jsx
+++ packages/tracking/src/trigger.stories.jsx
@@ -1,3 +1,4 @@
+import { text } from '@root/.preview/src/modules/knobs';
 import Trigger from './trigger';
 import Provider from './provider';
 import { action } from '../../../.preview/src/modules/actions';
@@ -6,10 +7,11 @@ export default { title: 'Trigger', component: Trigger };
 
 export const Default = () => {
   const handleEvent = action('onEvent');
+  const event = text('event', 'pageload-trigger');
 
   return (
     <Provider onEvent={handleEvent}>
-      <Trigger event="pageload-trigger" additional-data={123} />
+      <Trigger event={event} additional-data={123} />
       Triggers event on page-load. Check the console!
     </Provider>
   );
