2df9bb8f60e07b11191c37d02ad117d8aa3688f5
Nico Beierle <nico.beierle@gmail.com>
2021-09-22 13:25:08 +0200

    add chargebee step constants

--- packages/chargebee/src/constants/index.jsx
+++ packages/chargebee/src/constants/index.jsx
@@ -4,3 +4,7 @@ export const SECTION_BILLING_ADDRESS = 'EDIT_BILLING_ADDRESS';
 export const SECTION_ADD_PAYMENT = 'ADD_PAYMENT_SOURCE';
 export const SECTION_EDIT_PAYMENT = 'EDIT_PAYMENT_SOURCE';
 export const SECTION_VIEW_PAYMENT = 'VIEW_PAYMENT_SOURCE';
+export const STEP_CART_SCREEN = 'cart_screen';
+export const STEP_ADD_PAYMENT_METHOD = 'add_payment_method';
+export const STEP_REVIEW_SCREEN = 'review_screen';
+export const STEP_THANKYOU_SCREEN = 'thankyou_screen';
