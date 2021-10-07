76d1b3cd84df1631d6bfee98dd143b79d357bf18
Sergei Ozerov <ozerovsa@gmail.com>
2021-10-07 12:27:43 +0300

    Components: Add onScroll prop to SideScroller (#122)
    * Added onShift property for sideScroller
    
    * Added onDragStart property for sideScroller
    
    * Package lock
    
    * Change handler
    
    * Remove lockfile update release version
    
    Co-authored-by: Elias Braun <elias.braun@nebenan.de>

--- packages/components/src/side_scroller/index.stories.jsx
+++ packages/components/src/side_scroller/index.stories.jsx
@@ -1,9 +1,10 @@
+import { action } from '@root/.preview/src/modules/actions';
 import SideScroller from './index';
 
 export default { title: 'SideScroller', component: SideScroller };
 
 export const Default = () => (
-  <SideScroller>
+  <SideScroller onScroll={action('onScroll')}>
     <img
       src="https://www.west-crete.com/images/panoramas/house-view.jpg"
       height="300px" alt=""
