485a6e73f1b11a67e8dd9b4b028aced3df1368a5
Peter Haßler <pet.hassler@gmail.com>
2021-12-22 09:50:21 +0100

    Add story to replicate bug

---
+++ packages/modals/src/modal/scroll_lock.stories.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { arrayOf } from 'nebenan-helpers/lib/data';
+import Modal from './index';
+import styles from './scroll_lock.stories.module.scss';
+import ModalProvider from '../provider';
+
+
+export default { title: 'Modal - Scroll Lock', component: Modal, layout: false };
+
+export const Default = () => {
+  const [activeModal, setActiveModal] = useState(null);
+
+  return (
+    <ModalProvider>
+      <h1>Scroll down!</h1>
+
+      <section className={styles.overlay}>
+        <h1>This thing is here so that you can scroll down</h1>
+
+        {arrayOf(50).map(() => (
+          <p>Lorem ipsum</p>
+        ))}
+
+        <span className="ui-button ui-button-primary" onClick={setActiveModal.bind(undefined, 1)}>
+          open modal
+        </span>
+      </section>
+      {activeModal === 1 && (
+      <Modal
+        onClose={setActiveModal.bind(undefined, null)}
+        key="force-remount1"
+      >
+        <div className="ui-card">
+          This is the very first modal
+
+        </div>
+        <span className="ui-button ui-button-primary" onClick={setActiveModal.bind(undefined, 2)}>
+          open second modal
+        </span>
+      </Modal>
+      )}
+
+      {activeModal === 2 && (
+      <Modal
+        onClose={setActiveModal.bind(undefined, null)}
+        key="force-remount2"
+      >
+        <div className="ui-card">
+          This is the LAST modal!
+        </div>
+      </Modal>
+      )}
+
+    </ModalProvider>
+  );
+};
