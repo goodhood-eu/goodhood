32bfe4a5db555a41259f26b1987a0a5f49bb1bdf
eliasbraun <elias.braun@nebenan.de>
2022-01-10 12:06:05 +0000

    Deploying to gh-pages from @ goodhood-eu/goodhood@6e7120afedcb7ff9c4c8c27bf86121747fe9d900 🚀

---
+++ packages/components/src/ecosystem_bar/index.jsx
@@ -0,0 +1,45 @@
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { useRef } from 'react';
+import { useOnceSwipeTracking, useScrolled } from '@/src/ecosystem_bar/hooks';
+import styles from './index.module.scss';
+
+const EcosystemBar = ({ items, onFirstSwipe, onItemClick }) => {
+  const navRef = useRef(null);
+  const scrolled = useScrolled(navRef);
+
+  useOnceSwipeTracking(navRef, onFirstSwipe);
+
+  const renderItem = (item) => (
+    <li key={item.key} className={styles.item}>
+      <a
+        className={clsx(styles.link, { [styles.isActive]: item.isActive })}
+        href={item.link}
+        rel="noreferrer noopener"
+        target={item.isActive ? '_self' : '_blank'}
+        onClick={onItemClick.bind(undefined, item.event)}
+      >
+        <span className={styles.onlyMobile}>
+          {item.mobile}
+        </span>
+        <span className={styles.notMobile}>
+          {item.others}
+        </span>
+      </a>
+    </li>
+  );
+
+  return (
+    <ul className={clsx(styles.root, { [styles.scrolled]: scrolled })} ref={navRef}>
+      {items.map(renderItem)}
+    </ul>
+  );
+};
+
+EcosystemBar.propTypes = {
+  items: PropTypes.array.isRequired,
+  onFirstSwipe: PropTypes.func.isRequired,
+  onItemClick: PropTypes.func.isRequired,
+};
+
+export default EcosystemBar;
