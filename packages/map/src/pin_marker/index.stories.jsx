eddd98ec423bc15a5916cb7c95fae891e8519f4e
peterhass <34499633+peterhass@users.noreply.github.com>
2022-04-05 07:36:50 +0000

    Deploying to gh-pages from @ goodhood-eu/goodhood@3e290661109c3aa2350bc524356a30154d9ca062 🚀

--- packages/map/src/pin_marker/index.stories.jsx
+++ packages/map/src/pin_marker/index.stories.jsx
@@ -1,4 +1,4 @@
-import { boolean, select, text } from '@root/.preview/src/modules/knobs';
+import { select } from '@root/.preview/src/modules/knobs';
 import { action } from '@root/.preview/src/modules/actions';
 import config from '@root/config';
 import data from '../../sample_data';
@@ -31,15 +31,12 @@ export default { title: 'PinMarker', component: PinMarker };
 
 export const Default = () => {
   const type = select('Type', types, types[0]);
-  const popupContent = text('Popup Content', 'fire in the hole!');
-  const popupDefaultState = boolean('Popup default state', false);
 
   return (
     <Map credentials={config.map_credentials}>
       <PinMarker
         position={data.markers[0]}
-        {...{ type, popupContent, popupDefaultState }}
-        popupOffset={[-5, -30]}
+        type={type}
         onClick={action('Marker clicked')}
       />
     </Map>
