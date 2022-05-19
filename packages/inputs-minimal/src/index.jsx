53e74fe27c51c9022f5173cc249f6b904080e605
ozerovs <ozerovsa@gmail.com>
2022-05-19 12:34:35 +0000

    Deploying to gh-pages from @ goodhood-eu/goodhood@9655178aa5242f980f9640a25178f0e7de880c03 🚀

--- packages/inputs-minimal/src/index.jsx
+++ packages/inputs-minimal/src/index.jsx
@@ -3,3 +3,4 @@ export { default as Select } from './select';
 export { default as TextArea } from './textarea';
 export { default as TextField } from './textfield';
 export { default as RadioGroup } from './radio_group';
+export { default as PasswordField } from './password_field';
