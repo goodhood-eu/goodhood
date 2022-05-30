fe53b576b713a23800b58cb0054de1ebb87f4c81
Elias Braun <elias.braun@nebenan.de>
2022-05-30 10:57:28 +0200

    [CORE-9919] Introduce theme prop

--- packages/components/src/progress_bar/index.jsx
+++ packages/components/src/progress_bar/index.jsx
@@ -3,24 +3,25 @@ import clsx from 'clsx';
 import { getPercentageString } from './utils';
 import styles from './index.module.scss';
 
+export const THEME_MODERN = 'modern';
+
 const ProgressBar = ({
   state,
   size,
   children,
-  progressBarStyles,
-  progressBarProgressIndicatorStyles,
+  theme = THEME_MODERN,
   ...cleanProps
 }) => {
   const percent = getPercentageString(state);
 
-  const className = clsx(styles.root, progressBarStyles, {
+  const className = clsx(styles.root, styles[`theme-${theme}`], {
     [styles.small]: size === 'small',
   });
 
   return (
     <div {...cleanProps} className={className}>
       <span
-        className={clsx(styles.progressState, progressBarProgressIndicatorStyles)}
+        className={clsx(styles.progressState, styles[`theme-${theme}`])}
         style={{ width: percent }}
       >
         <em className={styles.percent}>{percent}</em>
@@ -39,6 +40,7 @@ ProgressBar.propTypes = {
   ]),
   size: PropTypes.string,
   children: PropTypes.node,
+  theme: PropTypes.object,
 };
 
 export default ProgressBar;
--- packages/components/src/progress_bar/index.stories.jsx
+++ packages/components/src/progress_bar/index.stories.jsx
@@ -1,18 +1,9 @@
 import ProgressBar from './index';
-import styles from './index.stories.module.scss';
 
 export default { title: 'ProgressBar', component: ProgressBar };
 
 export const Default = () => <ProgressBar state={55} />;
 
-
 export const Small = () => <ProgressBar state={34} size="small" />;
 
-export const CustomStyles = () => (
-  <ProgressBar
-    progressBarStyles={styles.progressBarStyles}
-    progressBarProgressIndicatorStyles={styles.progressBarProgressIndicatorStyles}
-    state={77}
-    size="small"
-  />
-);
+export const Legacy = () => <ProgressBar state={77} theme="legacy" />;
