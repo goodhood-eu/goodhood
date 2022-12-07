10828769fbd7c491789750559e898358b5afb234
veeck <michael.veeck@nebenan.de>
2022-12-07 11:34:09 +0100

    Dont noop, issue a warning about missing tracking context

--- packages/tracking/src/hooks.jsx
+++ packages/tracking/src/hooks.jsx
@@ -1,7 +1,6 @@
 import { useContext } from 'react';
 import Context from './context';
 
-const noop = () => {};
 
 export const useTracking = () => useContext(Context);
-export const useTrack = () => useTracking().track || noop;
+export const useTrack = () => useTracking().track || console.warn('No tracking context given!');
