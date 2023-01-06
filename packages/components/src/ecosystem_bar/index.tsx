e23b04776b0354dbb72c81e6cc978b8fb50ad112
veeck <michael.veeck@nebenan.de>
2023-01-06 13:44:52 +0100

    Start converting to typescript

--- packages/components/src/ecosystem_bar/index.tsx
+++ packages/components/src/ecosystem_bar/index.tsx
@@ -1,23 +1,39 @@
 import clsx from 'clsx';
-import PropTypes from 'prop-types';
 import { useRef } from 'react';
 import { useOnceSwipeTracking, useScrolled } from '@/src/ecosystem_bar/hooks';
 import styles from './index.module.scss';
 
-const EcosystemBar = ({ items, onFirstSwipe, onItemClick }) => {
+type EcosystemBarItem = {
+  key: string,
+  link: string,
+  isActive: boolean,
+  event: string,
+  text: {
+    mobile: string,
+    others: string,
+  },
+};
+
+export type EcosystemBarProps = {
+  items: EcosystemBarItem[];
+  onFirstSwipe: () => void;
+  onItemClick: (event: string) => void
+};
+
+export const EcosystemBar = ({ items, onFirstSwipe, onItemClick }: EcosystemBarProps) => {
   const navRef = useRef(null);
   const scrolled = useScrolled(navRef);
 
   useOnceSwipeTracking(navRef, onFirstSwipe);
 
-  const renderItem = (item) => (
+  const renderItem = (item:EcosystemBarItem) => (
     <li key={item.key} className={styles.item}>
       <a
         className={clsx(styles.link, { [styles.isActive]: item.isActive })}
         href={item.link}
         rel="noreferrer noopener"
         target={item.isActive ? '_self' : '_blank'}
-        onClick={onItemClick.bind(undefined, item.event)}
+        onClick={() => onItemClick(item.event)}
       >
         <span className={styles.onlyMobile}>
           {item.text.mobile}
@@ -34,12 +50,6 @@ const EcosystemBar = ({ items, onFirstSwipe, onItemClick }) => {
       {items.map(renderItem)}
     </ul>
   );
-};
-
-EcosystemBar.propTypes = {
-  items: PropTypes.array.isRequired,
-  onFirstSwipe: PropTypes.func.isRequired,
-  onItemClick: PropTypes.func.isRequired,
 };
 
 export default EcosystemBar;
