808422d692ff43ec1e14d67eee6fdbb6afb5a8cb
veeck <michael.veeck@nebenan.de>
2023-01-12 12:18:07 +0100

    Don't export types

--- packages/components/src/ecosystem_bar/index.tsx
+++ packages/components/src/ecosystem_bar/index.tsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import { useOnceSwipeTracking, useScrolled } from './hooks';
 import styles from './index.module.scss';
 
-export type EcosystemBarItem = {
+type EcosystemBarItem = {
   key: string,
   link: string,
   isActive: boolean,
@@ -14,7 +14,7 @@ export type EcosystemBarItem = {
   },
 };
 
-export type EcosystemBarProps = {
+type EcosystemBarProps = {
   items: EcosystemBarItem[];
   onFirstSwipe: () => void;
   onItemClick: (event: string) => void
