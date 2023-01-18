52951d6805c81ee28ffe708f6dc76ac5f7312dd9
Peter Haßler <pet.hassler@gmail.com>
2023-01-18 09:45:11 +0100

    Fix externals in rollup config

--- presets/react/rollup.config.js
+++ presets/react/rollup.config.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import resolve from '@rollup/plugin-node-resolve';
@@ -30,93 +29,102 @@ const move = (source, target) => ({
   },
 });
 
-export default (pkg, pkgPath) => ({
-  input: 'src/index',
-  acorn: { jsx: true },
-  acornInjectPlugins: [acornJsx()],
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named',
-    },
-  ],
-  plugins: [
-    commonjs(),
-    del({
-      targets: [
-        path.join(pkgPath, 'lib/*'),
-        path.join(pkgPath, 'styles.css'),
-      ],
-    }),
-    alias({
-      entries: [
-        { find: '@', replacement: pkgPath },
-        { find: '@root', replacement: ROOT_PKG_PATH },
-      ],
-    }),
-    peerDepsExternal(),
-    postcss({
-      extract: 'styles.css',
-      modules: {
-        globalModulePaths: [/node_modules/],
-        generateScopedName: `${kebapCase(pkg.name)}__[hash:base64:5]`,
-      },
-      use: [['sass', {
-        includePaths: [
-          path.join(pkgPath, 'node_modules/'),
-          path.join(ROOT_PKG_PATH, 'node_modules/'),
-        ],
-        functions: sassFunctions({ sass }),
-        sourceComments: true,
-      }]],
-      plugins: [
-        postcssPresetEnv(),
-      ],
-    }),
-    typescript({
-      cwd: pkgPath,
-      useTsconfigDeclarationDir: true,
-      exclude: ['**/*.stories.tsx'],
-      tsconfigOverride: {
-        // needed for package scoped type definition paths
-        rootDir: path.join(pkgPath, 'src/'),
-      },
-    }),
-    babel({
-      babelHelpers: 'runtime',
-      rootMode: 'upward',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      exclude: [
-        /node_modules/,
-      ],
-    }),
-    resolve({
-      browser: true,
-      extensions: ['.js', '.jsx', '.ts', '.tsx', 'json'],
-    }),
-    globImport({
-      format: 'mixed',
-      rename: (name, id) => {
-        if (name) return name;
-        if (/index\.js(x?)$/.test(id)) {
-          const dirname = path.basename(path.dirname(id));
-          return upperFirst(camelCase(dirname));
-        }
+export default (pkg, pkgPath) => {
+  const dependencies = [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ];
 
-        // not supported
-        return null;
+  return ({
+    input: 'src/index',
+    acorn: { jsx: true },
+    acornInjectPlugins: [acornJsx()],
+    output: [
+      {
+        file: pkg.module,
+        format: 'es',
+        sourcemap: true,
       },
-    }),
-    json(),
-    url(),
-    svgr(),
-    move(path.join(pkgPath, 'lib/styles.css'), path.join(pkgPath, 'styles.css')),
-  ],
-});
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+      },
+    ],
+
+    external: (id) => dependencies.some((depName) => id.startsWith(depName)),
+
+    plugins: [
+      commonjs(),
+      del({
+        targets: [
+          path.join(pkgPath, 'lib/*'),
+          path.join(pkgPath, 'styles.css'),
+        ],
+      }),
+      alias({
+        entries: [
+          { find: '@', replacement: pkgPath },
+          { find: '@root', replacement: ROOT_PKG_PATH },
+        ],
+      }),
+      postcss({
+        extract: 'styles.css',
+        modules: {
+          globalModulePaths: [/node_modules/],
+          generateScopedName: `${kebapCase(pkg.name)}__[hash:base64:5]`,
+        },
+        use: [['sass', {
+          includePaths: [
+            path.join(pkgPath, 'node_modules/'),
+            path.join(ROOT_PKG_PATH, 'node_modules/'),
+          ],
+          functions: sassFunctions({ sass }),
+          sourceComments: true,
+        }]],
+        plugins: [
+          postcssPresetEnv(),
+        ],
+      }),
+      typescript({
+        cwd: pkgPath,
+        useTsconfigDeclarationDir: true,
+        exclude: ['**/*.stories.tsx'],
+        tsconfigOverride: {
+          // needed for package scoped type definition paths
+          rootDir: path.join(pkgPath, 'src/'),
+        },
+      }),
+      babel({
+        babelHelpers: 'runtime',
+        rootMode: 'upward',
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        exclude: [
+          /node_modules/,
+        ],
+      }),
+      resolve({
+        browser: true,
+        extensions: ['.js', '.jsx', '.ts', '.tsx', 'json'],
+      }),
+      globImport({
+        format: 'mixed',
+        rename: (name, id) => {
+          if (name) return name;
+          if (/index\.js(x?)$/.test(id)) {
+            const dirname = path.basename(path.dirname(id));
+            return upperFirst(camelCase(dirname));
+          }
+
+          // not supported
+          return null;
+        },
+      }),
+      json(),
+      url(),
+      svgr(),
+      move(path.join(pkgPath, 'lib/styles.css'), path.join(pkgPath, 'styles.css')),
+    ],
+  });
+};
