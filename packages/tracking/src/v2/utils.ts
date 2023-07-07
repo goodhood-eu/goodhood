baa5964ed7c21afada2b4096ec6439cf1462e956
Florian Weinrich <florian@weinrich.dev>
2023-07-07 14:48:46 +0200

    Changes by jude

--- packages/tracking/src/v2/utils.ts
+++ packages/tracking/src/v2/utils.ts
@@ -7,3 +7,9 @@ export const setup = () => {
     event: 'gtm.js',
   });
 };
+
+export const getScriptSource = (id: string) => {
+  const source = new URL('/gtm.js', 'https://www.googletagmanager.com');
+  source.searchParams.set('id', id);
+  return source.toString();
+};
