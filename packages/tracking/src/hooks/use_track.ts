99d7c6f69bbe15a701ef30e8d8faa349f671d1e3
Florian Weinrich <florian@weinrich.dev>
2023-07-12 11:27:34 +0200

    fix: function overloading

--- packages/tracking/src/hooks/use_track.ts
+++ packages/tracking/src/hooks/use_track.ts
@@ -9,7 +9,7 @@ import {
   ReactEvent,
   SearchEvent,
   SwipeEvent,
-  ViewEvent,
+  ViewEvent, UnknownEvent,
 } from '../types';
 import { useLocation } from 'react-router';
 
@@ -17,29 +17,22 @@ declare const window: Window & {
   dataLayer: Record<string, unknown>[];
 };
 
-export const TrackingEvents = {
-  ClickEvent: 'gav4.clickEvent',
-  PageViewEvent: 'gav4.pageViewEvent',
-  RemoveEvent: 'gav4.removeEvent',
-  EditEvent: 'gav4.editEvent',
-  SucessFailEvent: 'gav4.success_failEvent',
-  ReactEvent: 'gav4.reactEvent',
-  SearchEvent: 'gav4.searchEvent',
-  SwipeEvent: 'gav4.swipeEvent',
-  ViewEvent: 'gav4.viewEvent',
-} as const;
 
-type TrackFunction = {
-  (event: 'gav4.clickEvent', payload: ClickEvent): void;
-  (event: 'gav4.pageViewEvent', payload: PageViewEvent): void;
-  (event: 'gav4.removeEvent', payload: RemoveEvent): void;
-  (event: 'gav4.editEvent', payload: EditEvent): void;
-  (event: 'gav4.success_failEvent', payload: SuccessFailEvent): void;
-  (event: 'gav4.reactEvent', payload: ReactEvent): void;
-  (event: 'gav4.swipeEvent', payload: SwipeEvent): void;
-  (event: 'gav4.viewEvent', payload: ViewEvent): void;
-  (event: 'gav4.searchEvent', payload: SearchEvent): void;
+type TrackEventMap = {
+  'gav4.clickEvent': ClickEvent;
+  'gav4.pageViewEvent': PageViewEvent;
+  'gav4.removeEvent': RemoveEvent;
+  'gav4.editEvent': EditEvent;
+  'gav4.success_failEvent': SuccessFailEvent;
+  'gav4.reactEvent': ReactEvent;
+  'gav4.searchEvent': SearchEvent;
+  'gav4.swipeEvent': SwipeEvent;
+  'gav4.viewEvent': ViewEvent;
+  'gav4.unknownEvent': UnknownEvent;
 };
+
+type TrackFunction = <K extends keyof TrackEventMap>(event: K, payload: TrackEventMap[K]) => void;
+
 
 export const useTrack = (): TrackFunction => {
   const { baseEvent, pageMapping } = useAnalytics();
