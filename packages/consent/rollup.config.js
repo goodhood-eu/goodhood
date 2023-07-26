6439fafa49772fc95f7f63c7707d8521adf9d07a
Florian Weinrich <florian@weinrich.dev>
2023-07-26 09:19:48 +0200

    remove consent

--- packages/consent/rollup.config.js
+++
@@ -1,4 +0,0 @@
-import pkg from './package.json';
-import getConfig from '../../presets/react/rollup.config';
-
-export default getConfig(pkg, __dirname);
--- packages/consent/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const VENDORS = [
-  'Google Tag Manager',
-  'Google Analytics',
-] as const;
