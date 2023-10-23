2bf3a49113615e0200494378d376ed8d42f401ac
Inspel <35484675+Inspel@users.noreply.github.com>
2023-10-23 14:26:53 +0000

    Deploying to gh-pages from @ goodhood-eu/goodhood@d9150f3c35023059e6374f8df5b71b9b8950c40b 🚀

---
+++ packages/tracking/webflow/webflow_ga_tracking_snippet.js
@@ -0,0 +1,53 @@
+// This is used in the Webflow projects and is not a part of the tracking module.
+
+const GA_PREFIX = 'data-ga-';
+
+window.dataLayer = window.dataLayer || [];
+
+const gatherData = (element) => {
+  const result = {};
+
+  if (!element.hasAttributes()) {
+    return result;
+  }
+
+  for (const attrib of element.attributes) {
+    if (attrib.name.startsWith(GA_PREFIX)) {
+      const attributeName = attrib.name.replace(GA_PREFIX, '').replace(/-/g, '_');
+
+      if (!attributeName) continue;
+
+      let parsedValue;
+      try {
+        parsedValue = JSON.parse(attrib.value);
+      } catch (e) {
+        parsedValue = attrib.value;
+      }
+      result[attributeName] = parsedValue;
+    }
+  }
+
+  return result;
+};
+
+
+const sendEvent = (event, payload) => {
+  window.dataLayer.push({ event, ...payload });
+};
+
+const handleDataAttributeClicks = (event) => {
+  const payload = gatherData(event.target);
+
+  if (Object.keys(payload).length) {
+    sendEvent('click', payload);
+  }
+};
+
+window.addEventListener('load', () => {
+  const onLoadPayload = gatherData(document.querySelector('body'));
+  if (Object.keys(onLoadPayload).length) {
+    sendEvent('page_view', onLoadPayload);
+  }
+
+  document.addEventListener('click', handleDataAttributeClicks);
+});
